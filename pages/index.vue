5136decb964b2e34e489977fa9d9099ddbe74c5f
peterkovacik <peter.a.kovacik@gmail.com>
2023-06-16 12:25:14 -0500

    add banner to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
+    <div class="green-mountain-banner d-flex justify-content-center align-items-center">
+        <h1 class="text-center text-white py-4 ">Arabic with Omar</h1>
+    </div>
+    
     <div >
         <!-- <img src="/images/classical-arabic-logo.jpeg" alt="" width="350px" height="350px" class=""> -->
-        <h1 class="text-center py-4">Arabic with Omar</h1>
+        <h1 class="text-center align-items-center">Arabic with Omar</h1>
     </div>
 </template>
 
@@ -12,5 +16,19 @@
 </script>
 
 <style scoped>
-    
+    .green-mountain-banner {
+        background-image: url('/images/green-mountain-unsplash.jpg');
+        height: 400px;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        border-bottom: #eee 1px solid;   
+    }
+
+    @media only screen and (min-width: 720px) {
+        .green-mountain-banner {
+            height: 650px;
+        }
+    }
+   
 </style>

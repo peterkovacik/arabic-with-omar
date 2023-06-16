233f17428fb0eacca8ab51a186ae3a03f8d0e1f8
peterkovacik <peter.a.kovacik@gmail.com>
2023-06-16 11:55:33 -0500

    change title on landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div >
         <!-- <img src="/images/classical-arabic-logo.jpeg" alt="" width="350px" height="350px" class=""> -->
-        <h1 class="text-center py-4">Home Page</h1>
+        <h1 class="text-center py-4">Arabic with Omar</h1>
     </div>
 </template>
 

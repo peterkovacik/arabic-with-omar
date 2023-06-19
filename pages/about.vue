d843c2ecf62f70cfb23a18261de00087a70cea8f
peterkovacik <peter.a.kovacik@gmail.com>
2023-06-18 23:20:22 -0500

    delete blog and add about page

---
+++ pages/about.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1 class="text-center  pt-4 pb-4">About Omar</h1>
+        <div class="content-width px-5 mb-5">
+            <p class="py-2">Omar became Muslim in 2001 and began studying Classical Arabic in 2005 at Qasid Institute in Jordan. He has a Bachelor of Arts in Linguistics from the University of Wisconsin-Milwaukee and certificates in Teaching English as a Foreign Language from the Wisconsin ESL Institute (WESLI) in Madison, WI. He taught English to non-native speakers in Aden, Yemen and Bursa, Turkey, he taught Arabic and ESL at Clara Mohammed School in Milwaukee, WI, and he designed and taught a program for Classical Arabic at Fox Valley Islamic Society in Neenah, WI. He is currently pursuing a Masters of Divinity in Islamic Chaplaincy at The Islamic Seminary of America (TISA) and taking additional coursework at Basira Education, Fayda Institute, and the Maliki School.</p>
+            <figure class="mt-3 mb-3">
+                <img src="/images/self-image-green-shirt.jpeg" alt="image of Omar"  class="center-image self-image"> 
+            </figure>
+            <p class="py-2">Omar's teachers in the Arabic language include: Ustadh Amer Melham, Shaykh Faraz Khan, Shaykh Sohail Hanif (Qasid Institute); Dr. Hamid Ouali (University of Wisconsin-Milwaukee); Mufti Yusuf Mullan (Toronto Shariah Program); Ustadh Farid Dingle (Qibla); Ustadh Muhammad Metlwey (Studio Arabiyyah); Ustadh Sayed Fathy (Sibawyah Institute); and Mawlana Syed Javed Ali Shah (private lessons).</p>
+            <p class="py-2">His teachers in Islamic Studies include: Hafiz M. Shafique (Masjid al-Qur'an in Milwaukee); Shaykh Salah al-Kurdi (Dar al-Hamd); Shaykh Ali Hani (private lessons); Shaykh Hamza Karamali (Sunnipath and Basira Education); Shaykh Abdul-Karim Yahya, Shaykh Faraz Rabbani (Sunnipath); Dr. Ashraf Muneeb (Qibla); Ustadh Abdo (Masjid al-'Aydarous in Aden); Mawlana Syed Javed Ali Shah (private lessons); Shaykh Isa Johannsen (Madina Institute USA); Shaykh Mujahid Abdul-Karim (Fayda Institute); Shaykh Saifullah Abu M. (Maliki School); Dr. James Jones, Dr. Julie Lowe, Dr. Omar Imady, Dr. Tamara Gray, and Dr. Yasir Qadhi (The Islamic Seminary of America).</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+   .content-width {
+        max-width: 700px;
+        display: block;
+        margin: 0 auto;
+    }
+
+    .center-image {
+        display: block;
+        margin: 0 auto;
+    }
+
+    .self-image {
+        height: 400px;
+        width: 300px;
+    }
+
+    @media only screen and (min-width: 720px) {
+        .self-image {
+            height: 480px;
+            width: 360px;
+        }
+
+        
+    }
+</style>
